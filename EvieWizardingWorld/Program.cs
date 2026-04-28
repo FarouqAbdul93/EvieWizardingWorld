fe22b620d3f9bf9bb719d73bbe238b06971468ae
Farouq <farouqha93@gmail.com>
2026-04-28 14:25:55 +0100

    Task2 - complete

--- EvieWizardingWorld/Program.cs
+++ EvieWizardingWorld/Program.cs
@@ -2,6 +2,8 @@ using EvieWizardingWorld.Models;
 using EvieWizardingWorld.Services;
 using Microsoft.AspNetCore.Mvc;
 
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
 namespace EvieWizardingWorld
 {
     public class Program
@@ -13,9 +15,17 @@ namespace EvieWizardingWorld
             builder.Services.AddControllers();
             builder.Services.AddScoped<SpellsService>();
             builder.Services.AddScoped<SpellsModel>();
+            builder.Services.AddHealthChecks()
+                .AddCheck<TeacherHealthCheck>("teacher_file_health_check",
+                failureStatus: HealthStatus.Unhealthy,
+                tags: new[] { "file", "teachers" });
+
+
             var app = builder.Build();
 
             app.UseRouting();
+
+            app.UseHealthChecks("/health");
 
             app.UseEndpoints(endpoints =>
             {
---
+++ EvieWizardingWorld/TeacherHealthCheck.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Text.Json;
+
+namespace EvieWizardingWorld
+{
+    public class TeacherHealthCheck : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+           
+                string filePath = "Resources/Teachers.json";
+                string jsonText = await File.ReadAllTextAsync(filePath);
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var teachers = JsonSerializer.Deserialize<List<object>>(jsonText, options);
+
+                int teacherCount = teachers.Count();
+
+            if (teacherCount > 0)
+                {
+                    return HealthCheckResult.Healthy($"There are {teacherCount} teachers available.");
+                }
+                else
+                {
+                    return HealthCheckResult.Unhealthy($"There are {teacherCount} no teachers available.");
+                }
+            
+           
+        }
+    }
+}
